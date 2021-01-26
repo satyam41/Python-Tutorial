0e40179937439bbdcb7f89c83e96264c0390a138
Satyam <thesatyam10@gmail.com>
2021-01-26 20:14:43 +0530

    Add String files

---
+++ string.py
@@ -0,0 +1,23 @@
+a = 'I Love Coding'
+# print(type(a))
+
+l = ['P','Y','T','H','O','N']
+# print(type(l))
+string = str(l)
+# print(string)
+# print(type(string))
+
+num = 123
+srt = str(num)
+# print(srt)
+# print(type(srt))
+
+string = "i love coding"
+# print(string.capitalize())
+# print(string.upper())
+# print(string.lower())
+# print(string.join("*"+string+"*"))
+# print(type(string.split()))
+# print(string)
+# print(string.title())
+# print(len(string))
