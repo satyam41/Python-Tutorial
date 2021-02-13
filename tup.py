32bd9c78b4d2a305f11ed55e569febb8e9cd9d52
Satyam <thesatyam10@gmail.com>
2021-02-13 16:54:25 +0530

    Add a new tutorial tuple

---
+++ tup.py
@@ -0,0 +1,28 @@
+# (), immutable, datatype, (float, int, str, bool).
+
+tup = (1,2,3,4,5)
+# print(type(tup))
+
+tupl = (1,)
+# print(type(tupl))
+
+lst = [1,2,3,4,5] #ms word file.
+pdf = tuple(lst) # conver into pdf.
+# print(type(pdf))
+
+print(pdf)
+
+# print(pdf[2])
+
+# pdf.append(2)
+# pdf[3] = 7
+# print(pdf)
+
+# print(pdf.index(4))
+
+# tup = (2,2,5,6,6,5,5,2,2,6,6)
+# print(tup.count(2))
+
+tup = (4,6,5,2,1,4)
+# tup.sort()
+# print(tup)
