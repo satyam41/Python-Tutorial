ebaba474498342a69233ea04dd39c85fdaa8e0df
Satyam <thesatyam10@gmail.com>
2021-02-18 18:24:58 +0530

    add new file loop.py

---
+++ loop.py
@@ -0,0 +1,21 @@
+# for, while 
+# num = int(input("Enter any number: "))
+# for i in range(1,11):
+#     print(i*num)
+
+# i = 1
+# while (i<=10):
+#     print(i)
+#     i+=1
+
+# lst = [1,2,3,4,5,6,7,8,9,10]
+# for i in lst:
+#     print(i)
+
+# lst = []
+# for i in range(1,21):
+#     lst.append(i)
+# print(lst)
+
+# lst = [i*15 for i in range(1,11)]
+# print(lst)
